da3b2acb61fddcbccf2fb07828254e93b7b46ee2
VictorIranzo <viranzo96@gmail.com>
2018-04-25 17:49:14 +0200

    Primeros pasos Parte 4 Ejercicio 1.

---
+++ SMM_Practice3_Part4/Exercise 1/NetStatus/NetStatus/NetStatus/NoNetworkPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NetStatus
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NoNetworkPage : ContentPage
+	{
+		public NoNetworkPage  ()
+		{
+            BackgroundColor = Color.FromRgb(0xf0, 0xf0, 0xf0);
+            Content = new Label()
+            {
+                Text = "No network Connection Available",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                TextColor = Color.FromRgb(0x40,0x40,0x40)
+            };
+
+			InitializeComponent ();
+		}
+	}
+}
